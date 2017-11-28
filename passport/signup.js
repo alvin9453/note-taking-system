1aee49020a467459c0f37b184379f3a264fa8a20
alvin9453 <jiaweilin0923@gmail.com>
2017-11-28 13:01:58 +0800

    [MISC] Sign up with different character

--- passport/signup.js
+++ passport/signup.js
@@ -30,8 +30,7 @@ module.exports = function(passport){
                         newUser.username = username;
                         newUser.password = createHash(password);
                         newUser.email = req.param('email');
-                        newUser.firstName = req.param('firstName');
-                        newUser.lastName = req.param('lastName');
+                        newUser.character = req.param('character');
 
                         // save the user
                         newUser.save(function(err) {
